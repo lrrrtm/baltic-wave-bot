38a3308bde4aa7c81988d45b8dcfa3ab972baa0b
Artem Larionenko <lrrrtm.edu@yandex.ru>
2025-03-17 19:01:40 +0300

    init

--- utils/orders_checker.py
+++ utils/orders_checker.py
@@ -1,5 +1,9 @@
-import logging
+import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import logging
 import time
 from datetime import datetime
 
